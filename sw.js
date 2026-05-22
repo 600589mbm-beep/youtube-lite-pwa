8e851890d578b2f954f4a2c86aafcd9786fe06f2
600589mbm-beep <600589mbm@gmail.com>
2026-05-22 01:36:01 -0500

    Cache the VPS control room and landing page

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ const CACHE_NAME = "youtube-automation-agent-v1";
 
 const FILES_TO_CACHE = [
   "./",
+  "./app.html",
+  "./landing",
   "./index.html",
   "./manifest.webmanifest",
   "./icon.svg"
